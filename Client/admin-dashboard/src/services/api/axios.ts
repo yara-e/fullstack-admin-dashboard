c145776615c4289e0d000aea9ddded5299516062
yara-e <yaraelsayed237@gmail.com>
2026-02-24 02:05:05 +0200

    update logo

--- Client/admin-dashboard/src/services/api/axios.ts
+++ Client/admin-dashboard/src/services/api/axios.ts
@@ -2,10 +2,10 @@ import axios from "axios";
 import { getToken, isTokenExpired } from "../../utils/token";
 import { store } from "../../app/store"
 import { logout } from "@/features/auth/authSlice";
- 
+
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://fullstack-admin-dashboard-production.up.railway.app/",
 });
 
 api.interceptors.request.use((config) => {
@@ -13,7 +13,7 @@ api.interceptors.request.use((config) => {
 
   if (token) {
     if (isTokenExpired(token)) {
-       
+
       store.dispatch(logout());
       return config;
     }
@@ -25,8 +25,8 @@ api.interceptors.request.use((config) => {
 });
 
 api.interceptors.response.use(
-  
-  (res) => res ,
+
+  (res) => res,
   (error) => {
     if (error.response?.status === 401) {
       store.dispatch(logout());
